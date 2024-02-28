1fea2f90f0508d553b1db61a5dd17b112031c857
mihaelrisek <mihaelrisek2@gmail.com>
2024-02-28 19:51:45 +0100

    testing

--- server/models/database.js
+++ server/models/database.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () =>{
-  console.log('Connected');
-})
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('MongoDB connected');
+    
+    // Start server only after MongoDB connection is established
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
